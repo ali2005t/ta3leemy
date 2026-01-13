d78e3003682c8153449f9e88c3a495cb23c3fce8
ali2005t <lwlmho44@gmail.com>
2026-01-13 16:20:17 +0200

    feat: setup pyinstaller to build standalone exe

---
+++ builder_tool/build_exe.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Installing requirements...
+pip install -r requirements.txt
+
+echo Building EXE...
+pyinstaller --noconfirm --onefile --windowed --name "Ta3leemyBuilder" --add-data "customtkinter;customtkinter" main.py
+
+echo Done! You can find the EXE in the 'dist' folder.
+pause
